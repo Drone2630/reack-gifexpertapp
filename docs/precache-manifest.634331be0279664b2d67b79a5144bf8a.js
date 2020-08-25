4d0e4606fc6b358ff4fd89ab54ecb19bb6a009bf
Benito David Rojas Vargas <bdavid2nd@live.com>
2020-08-25 14:23:24 -0500

    docs agregada

---
+++ docs/precache-manifest.634331be0279664b2d67b79a5144bf8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5ca9eca0536ed31fc660572e218cfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8b7f6bf5ba256a36a5c",
+    "url": "/static/css/main.9718acd2.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b7f6bf5ba256a36a5c",
+    "url": "/static/js/main.6fb5f617.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
